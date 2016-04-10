e810225ae2ff0ebc01f2b5c40e8313faedb41203
hffeng <foreverfeng168@gmail.com>
2016-04-10 19:15:19 +0800

    scratch the data

--- src/main/java/com/feng/eastmoney/App.java
+++ src/main/java/com/feng/eastmoney/App.java
@@ -2,6 +2,7 @@ package com.feng.eastmoney;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
@@ -13,10 +14,19 @@ public class App
     public static void main ( String[] args ) throws Exception
     {
         Document doc = Jsoup.connect("http://data.eastmoney.com/stock/traderstatistic.html").get();
+       
         
-        Elements elements = doc.select("dt_1");
+        Elements trList = doc.getElementsByClass("tbody");
         
-        System.out.println(elements.size());
+        Elements tbody = doc.select("table").select("tbody").select("tr");
+        
+        for(Element e:tbody){
+        	Elements el = e.select("td");
+        	
+        	for(Element t:el){
+        		System.out.println(t.select("td").text());
+        	}
+        }
     }
     
     

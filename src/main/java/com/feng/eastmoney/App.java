91640366b85d252a9eb1022f598c29b55c35fe27
hffeng <foreverfeng168@gmail.com>
2016-04-10 18:29:37 +0800

    add jsoup files

--- src/main/java/com/feng/eastmoney/App.java
+++ src/main/java/com/feng/eastmoney/App.java
@@ -1,13 +1,23 @@
 package com.feng.eastmoney;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main ( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        Document doc = Jsoup.connect("http://data.eastmoney.com/stock/traderstatistic.html").get();
+        
+        Elements elements = doc.select("dt_1");
+        
+        System.out.println(elements.size());
     }
+    
+    
 }

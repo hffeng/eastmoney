de0c69b6fce33d6d6448ab8587d4b6255da725bf
hffeng <foreverfeng168@gmail.com>
2016-04-10 17:46:08 +0800

    first commit

---
+++ src/main/java/com/feng/eastmoney/App.java
@@ -0,0 +1,13 @@
+package com.feng.eastmoney;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

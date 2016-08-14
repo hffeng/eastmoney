49c5399f732cfc7a98d5bb8b790554860a47a562
fenghf <foreverfeng168@gmail.com>
2016-08-14 17:18:36 +0800

    Update App.java

--- src/main/java/com/feng/eastmoney/App.java
+++ src/main/java/com/feng/eastmoney/App.java
@@ -1,33 +1,50 @@
 package com.feng.eastmoney;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main ( String[] args ) throws Exception
-    {
-        Document doc = Jsoup.connect("http://data.eastmoney.com/stock/traderstatistic.html").get();
-       
-        
-        Elements trList = doc.getElementsByClass("tbody");
-        
-        Elements tbody = doc.select("table").select("tbody").select("tr");
-        
-        for(Element e:tbody){
-        	Elements el = e.select("td");
-        	
-        	for(Element t:el){
-        		System.out.println(t.select("td").text());
-        	}
-        }
-    }
-    
-    
+public class App {
+	public static void main(String[] args)  {
+
+		System.out.println("asdfa");
+		try {
+			String cont = "";
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			Date d = new Date();
+			while (d.getHours() < 24 && d.getHours() > 6) {
+
+				Document doc = Jsoup.connect("http://www.jiayuan.com/113296714").get();
+				Element ele = doc.select("div.my_info.yh.not_online").first();
+				Element ele1 = doc.select("div.on_line.yh").first();
+				if (ele != null) {
+					cont = ele.text();
+				} else if (ele1 != null) {
+					cont = ele1.text();
+				}
+
+				if (cont.contains("在线") && cont.indexOf("不在线") <= 0) {
+					System.out.println(df.format(new Date()));
+					FileAccess.method2("I://info2.txt", df.format(new Date()) + "\r\n");
+				}
+				Thread.sleep(200000);
+			}
+		} catch (Exception e) {
+			try{
+			  Thread.sleep(300000);
+			}catch(Exception ce){
+				App.main(new String[] {});
+			}
+			e.printStackTrace();
+			App.main(new String[] {});
+		}
+	}
 }
